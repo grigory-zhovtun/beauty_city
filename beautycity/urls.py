ad76be202f7a5e828e20aabf9967d6580f0feeb4
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-07-01 16:10:20 +0500

    Add `re_path` for flexible URL patterns, include `salon.urls` under `/api/v1/`, and enable media serving in debug mode

--- beautycity/urls.py
+++ beautycity/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from django.urls import path, include # <-- Добавь include
+from django.urls import path, include, re_path # <-- Добавь include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from bot.webhook import webhook
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webhook/', webhook),
+    re_path(r'^webhook/?
+    # Новая строка: все адреса из salon.urls будут доступны по префиксу /api/v1/
+    path('api/v1/', include('salon.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT), webhook),
     # Новая строка: все адреса из salon.urls будут доступны по префиксу /api/v1/
     path('api/v1/', include('salon.urls')),
 ]
