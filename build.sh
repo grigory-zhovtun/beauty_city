6b4af3f70a86a794210e8847a634c9fd493ff883
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-07-01 17:58:16 +0500

    Comment out redundant salon fake-initial migration in `build.sh` to streamline the process

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 # First, fake unapply the initial migration for salon to ensure all operations are re-run
-python manage.py migrate salon 0001_initial --fake-initial --fake
+#python manage.py migrate salon 0001_initial --fake-initial --fake
 # Then, apply all migrations
 python manage.py migrate
