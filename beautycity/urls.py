af81100249475f9924bee6c9828686384ed397b4
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-07-01 16:13:33 +0500

    Fix misplaced `re_path` declaration in `urls.py` and restore media serving in debug mode

--- beautycity/urls.py
+++ beautycity/urls.py
@@ -6,13 +6,7 @@ from bot.webhook import webhook
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^webhook/?
-    # Новая строка: все адреса из salon.urls будут доступны по префиксу /api/v1/
-    path('api/v1/', include('salon.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT), webhook),
+    re_path(r'^webhook/?, webhook),
     # Новая строка: все адреса из salon.urls будут доступны по префиксу /api/v1/
     path('api/v1/', include('salon.urls')),
 ]
